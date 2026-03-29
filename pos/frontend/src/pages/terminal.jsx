691d6aff7a0409d2187a6430780b649becb860c3
qiurina <libradillaeric116@gmail.com>
2026-03-29 14:00:44 +0800

    button change color

--- pos/frontend/src/pages/terminal.jsx
+++ pos/frontend/src/pages/terminal.jsx
@@ -1879,7 +1879,7 @@ const Terminal = () => {
                 onClick={() => setSelectedCategory(cat.name)}
                 className={`flex items-center justify-center px-6 py-3 rounded-xl text-sm font-bold transition-all duration-200 shadow-md border truncate ${
                   selectedCategory === cat.name
-                    ? `text-[#AD7F65] border-b-4 border-gray-200] ${
+                  ? `text-[#AD7F65] border-b-4 border-x-0 border-t-0 border-[#AD7F65] ${
                       theme === "dark" ? "bg-[#2A2724]" : "bg-white"
                     }`
                     : theme === "dark"
